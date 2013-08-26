d5fde03367844fca093fc2f399940805bc22b580
jony <jony.malhotra82@gmail.com>
2013-08-26 15:18:56 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbPayment::Application.config.secret_key_base = '166624130ba3785993455b353f15c36de39576ec64ad0a06d908e1f3d880b267b3664cc96589e5d0ddf044f89fa895d8b6eea37fe452e47efb7219e21e062487'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FbPayment::Application.config.session_store :cookie_store, key: '_fb_payment_session'
